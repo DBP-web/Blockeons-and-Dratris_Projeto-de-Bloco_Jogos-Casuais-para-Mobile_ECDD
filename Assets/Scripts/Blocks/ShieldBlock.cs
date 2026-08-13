4727e1de6c0228050fbf9677129ebff637a1e973
Wilber <wilber.sa@hotmail.com>
2026-08-13 19:34:06 -0300

    feat: criação da pasta com as lógicas dos blocos
    Pasta contendo os diferentes blocos do jogo, separados entre ofensivos, defensivos e de suporte, todos herdando informações básicas do BlockBase.

---
+++ Assets/Scripts/Blocks/ShieldBlock.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace BlockeonsDratris.Blocks
+{
+    public class ShieldBlock : BlockBase
+    {
+        [Header("Valor de Escudo")]
+        [Tooltip("Quanto de dano esse bloco absorve/reduz quando quebrado, se aplicável no futuro")]
+        public int shieldValue = 5;
+
+        public override void Setup(int col, int r)
+        {
+            base.Setup(col, r);
+            category = BlockCategory.Shield;
+        }
+    }
+}
